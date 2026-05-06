cf28df25d4216b171cd9cfb42596fdbdf37123ba
rrosnik <RezaRostaminikoo@gmail.com>
2026-05-05 23:40:46 -0400

    set up dashboard and auth pages
    - added sidebar and header
    - added auth components from better-auth-ui
    - added tanstack query and beter auth provider to the app layout
    - added dashboard components: sidebar and header

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,8 @@
-export default async function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
-  );
+
+export default function Home() {
+	return (
+		<div className="">
+			Dashboard
+		</div>
+	);
 }
