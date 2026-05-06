101a99ae36b7d9ba570ec0e8926631a7477b7893
rrosnik <RezaRostaminikoo@gmail.com>
2026-05-06 01:19:28 -0400

    set up dashboard and auth pages
    - added sidebar and header
    - added auth components from better-auth-ui
    - added tanstack query and beter auth provider to the app layout
    - added dashboard components: sidebar and header

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,18 +1,12 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { DashboardNavbar } from "@/modules/dashboard/ui/components/dashboard-navbar";
-import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+import { DashboardLayoutView } from "@/modules/dashboard/ui/views/dashboard-layout-view";
 import React from "react";
 
-const DashboardLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return (
-    <SidebarProvider>
-      <DashboardSidebar />
-      <main className="flex flex-col flex-1 min-h-svh w-screen bg-muted">
-        <DashboardNavbar />
-        {children}
-      </main>
-    </SidebarProvider>
-  );
+const DashboardLayout = ({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) => {
+	return <DashboardLayoutView>{children}</DashboardLayoutView>;
 };
 
 export default DashboardLayout;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -38,4 +38,15 @@ export const auth = betterAuth({
 		},
 		expiresIn: 3600, // in seconds
 	},
+
+	socialProviders: {
+		github: {
+			clientId: process.env.GITHUB_CLIENT_ID!,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+		},
+		google: {
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		},
+	},
 });
