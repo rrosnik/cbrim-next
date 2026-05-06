1c261c0a353220a3e71f09ae6a5fd6c2223e7933
rrosnik <RezaRostaminikoo@gmail.com>
2026-05-06 18:22:02 -0400

    patched

--- src/app/api/resend/route.ts
+++ src/app/api/resend/route.ts
@@ -1,19 +1,3 @@
-// import { resend } from "@/lib/email";
-// import { EmailTemplate } from "@/modules/email/templates/sample";
-
 export async function POST() {
-	try {
-		// const { data, error } = await resend.emails.send({
-		// 	from: "Acme <onboarding@rosnik.dev>",
-		// 	to: ["rezarostaminikoo@gmail.com"],
-		// 	subject: "Hello world",
-		// 	react: EmailTemplate({ firstName: "John" }),
-		// });
-		// if (error) {
-		// 	return Response.json({ error }, { status: 500 });
-		// }
-		// return Response.json(data);
-	} catch (error) {
-		return Response.json({ error }, { status: 500 });
-	}
+	return Response.json({ error: "Not implemented" }, { status: 501 });
 }
--- src/modules/dashboard/type.d.ts
+++ src/modules/dashboard/type.d.ts
@@ -1,8 +1,11 @@
+import React from "react";
+
 declare global {
 	namespace Sidebar {
 		type SidebarMenuItem = {
 			label: string;
 			url: string;
+			Icon: React.ReactNode;
 		};
 		type SidebarGroup = {
 			label: string;
